e12b6d446fcc982c27ab1ee16a316d6aad3ab922
Paweł <pawel_gnat@o2.pl>
2022-07-26 12:30:48 +0200

    added readme, added few classes to toggle theme mode, finished javascript

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-Under development
+# Frontend Mentor - Social media dashboard with theme switcher solution
+
+This is a solution to the [Social media dashboard with theme switcher challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [Social media dashboard with theme switcher](https://pawel-gnat.github.io/Frontend-Mentor-Social-media-dashboard/)
+
+## My process
+
+Java Script:
+
+1. Created variables,
+2. Made 3 loop functions with array variables to toggle classlist 'white',
+3. Made event listener on theme switcher to toggle white class with the rest of variables.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+Challenge completed within 13,5 hours. I learned how to toggle theme mode and I made a nice looking flexbox sections. I like how they response to bigger width.
+
+## Author
+
+- Frontend Mentor - [@Pawel-Gnat](https://www.frontendmentor.io/profile/Pawel-Gnat)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 const switchThemeInput = document.querySelector('#themeSwitch') // my input theme switcher
 const body = document.querySelector('body')
+const headingAndNumberTexts = document.querySelectorAll('.general-text') // selected all my white card texts and white main heading
+const secondaryInfoTexts = document.querySelectorAll('.secondary-text') // selected all my gray color texts, also inside cards
+const headingBackground = document.querySelector('.heading-bg')
+const switchLabel = document.querySelector('.theme-switch-label')
+const switchKnob = document.querySelector('.theme-switch-switcher')
+const SocialCards = document.querySelectorAll('.card') // selected all my media cards
+const overviewHeading = document.querySelector('.overview__heading') // overview - today text
+
+function changeWhiteText() {
+	for (let i = 0; i < headingAndNumberTexts.length; i++) {
+		headingAndNumberTexts[i].classList.toggle('white')
+	}
+}
+
+function changeGrayText() {
+	for (let i = 0; i < secondaryInfoTexts.length; i++) {
+		secondaryInfoTexts[i].classList.toggle('white')
+	}
+}
+
+function changeCardsColor() {
+	for (let i = 0; i < SocialCards.length; i++) {
+		SocialCards[i].classList.toggle('white')
+	}
+}
 
 switchThemeInput.addEventListener('click', e => {
 	body.classList.toggle('white')
+	headingBackground.classList.toggle('white')
+	changeWhiteText()
+	changeGrayText()
+	switchLabel.classList.toggle('white')
+	switchKnob.classList.toggle('white')
+	changeCardsColor()
+	overviewHeading.classList.toggle('white')
 })
+ 
